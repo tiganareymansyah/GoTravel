1b40280a595224b8f350e31dc4e03102323a0c56
Tigana Reymansyah <tiganareymansyah2502@gmail.com>
2024-05-23 21:05:43 +0700

    23 Mei 2024
    Perbaikan tampilan page booking

--- frontend-react/src/pages/layouts/booking/Booking.jsx
+++ frontend-react/src/pages/layouts/booking/Booking.jsx
@@ -1,7 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import { Add } from '@mui/icons-material';
 import Navbar from "../../../components/navbar/Navbar";
 import { useMediaQuery } from "react-responsive";
 import { useBookingStyles } from "./style";
+import { orange } from "@mui/material/colors";
 
 export default function Booking(props) {
     console.log(props);
@@ -9,11 +11,97 @@ export default function Booking(props) {
     const isMobile = useMediaQuery({ maxWidth: 991 });
     const classes = useBookingStyles({ isMobile });
 
+    const styles = {
+        buttonTambah: {
+            padding: "7px 14px",
+            textAlign: "center",
+            cursor: "pointer",
+            borderRadius: "8px",
+            color: "black",
+            backgroundColor: "#fff",
+            "&:hover": {
+                color: orange[100],
+            },
+        },
+
+        stackPagination: {
+            paddingTop: "16px", 
+            "& .MuiPagination-root": {
+                display: "flex", 
+                justifyContent: "end" 
+            }
+        },
+
+        pagination: {
+            "& .MuiPaginationItem-root": {
+                color: orange[100], 
+                borderColor: orange[100], 
+                "&.Mui-selected": {
+                    color: "#000", 
+                    backgroundColor: "#fff", 
+                    borderColor: "#fff", 
+                },
+            }
+        },
+    };
+
+    let data = "ada";
+
     return (
         <>
             <Box className={classes.bookingBackground}>
                 <Navbar />
-                <Box>Booking</Box>
+                <Box className={classes.containerParent}>
+                    <Box className={classes.setCardAndButton}>
+                        <Box className={classes.buttonTambah}>
+                            <Button
+                                sx={styles.buttonTambah}
+                                startIcon={<Add />}
+                                // onClick={(e) => {
+                                //     e.preventDefault();
+                                //     navigate("/");
+                                // }}
+                            >
+                                Tambah
+                            </Button>
+                        </Box>
+
+                        {data === "ada" ? (
+                            <>
+                                <Box className={classes.setCard}>
+                                    <Box className={classes.boxCard}>
+                                        Data 1
+                                    </Box>
+                                    <Box className={classes.boxCard}>
+                                        Data 2
+                                    </Box>
+                                    <Box className={classes.boxCard}>
+                                        Data 3
+                                    </Box>
+                                </Box>
+                                <Stack spacing={2} sx={styles.stackPagination}>
+                                    <Pagination 
+                                        count={10} 
+                                        variant="outlined" 
+                                        sx={styles.pagination}
+                                    />
+                                </Stack>
+                            </>
+                        ) : (
+                            <Box className={classes.boxNoData}>
+                                <Typography 
+                                    variant="body2" 
+                                    gutterBottom // property gutterBottom pada component Typography berfungsi membuat marginBottom sebesar 0.35em jika ada component lain
+                                    sx={{ color: "#fff", fontStyle: "italic", letterSpacing: "2px" }}
+                                >
+                                    Tidak ada data booking.
+                                </Typography>
+                            </Box>
+                        )}
+
+                    </Box>
+
+                </Box>
             </Box>
         </>
     );
--- frontend-react/src/pages/layouts/booking/style.js
+++ frontend-react/src/pages/layouts/booking/style.js
@@ -3,10 +3,48 @@ import background from "../../../media/sean-oulashin-KMn4VEeEPR8-unsplash.jpg";
 
 export const useBookingStyles = makeStyles({
     bookingBackground: {
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${background})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        minHeight: "100vh"
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${background})`, 
+        backgroundSize: "cover", 
+        backgroundRepeat: "no-repeat", 
+        backgroundPosition: "center", 
+        minHeight: "100vh" 
+    },
+
+    containerParent: {
+        width: "100%" 
+    },
+
+    setCardAndButton: {
+        height: "72vh", 
+        width: "85%", 
+        margin: "auto" 
+    },
+
+    setCard: {
+        display: "flex", 
+        justifyContent: "space-between", 
+        alignItems: "center"
+    },
+
+    boxCard: {
+        padding: "16px", 
+        boxSizing: "border-box", 
+        width: "400px", 
+        height: "400px", 
+        borderRadius: "8px", 
+        backgroundColor: "#fff",
+        boxShadow: "8px 8px 0 rgba(0, 0, 0, 0.5)"
+    },
+
+    buttonTambah: {
+        paddingTop: "32px", 
+        display: "flex", 
+        justifyContent: "end", 
+        marginBottom: "16px" 
+    },
+
+    boxNoData: {
+        display: "flex", 
+        justifyContent: "center" 
     },
 });
